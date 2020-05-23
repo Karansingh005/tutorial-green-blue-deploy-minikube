550b626814a07699c64cecd47c625a874e5a7430
Karan Singh <karansinghreen@gmail.com>
2020-05-23 15:00:35 +0530

    Update upload_docker.sh

--- blue/upload_docker.sh
+++ blue/upload_docker.sh
@@ -11,8 +11,8 @@ dockerpath=testblueimage
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker login --username andresaaap
-docker tag testblueimage andresaaap/testblueimage
+docker login --username karansingh005
+docker tag testblueimage karansingh005/testblueimage
 # Step 3:
 # Push image to a docker repository
-docker push andresaaap/testblueimage
+docker push karansingh005/testblueimage
